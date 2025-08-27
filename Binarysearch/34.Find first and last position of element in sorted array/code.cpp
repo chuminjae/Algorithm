c80936726a1ab50b5be20974df68f5532d6285bc
c_vgvrthrg <choochootrain1130@gmail.com>
2025-08-28 01:48:32 +0900

    Create code.cpp

---
+++ Binarysearch/34.Find first and last position of element in sorted array/code.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = -1;
+        bool check = false;
+        vector<int> a;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] >= target) {
+                if (nums[mid] == target) {
+                    check = true;
+                }
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        if (check) {
+            a.push_back(start);
+        } else {
+            a.push_back(-1);
+        }
+        start = 0;
+        end = nums.size() - 1;
+        check = false;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] <= target) {
+                if (nums[mid] == target) {
+                    check = true;
+                }
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        if (check) {
+            a.push_back(end);
+        } else {
+            a.push_back(-1);
+        }
+        return a;
+    }
+};
