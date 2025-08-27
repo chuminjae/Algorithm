5a2bb243b309f857ee43bd2c7e33ad72b440ece2
c_vgvrthrg <choochootrain1130@gmail.com>
2025-08-27 22:21:43 +0900

    Create code.cpp

---
+++ Binarysearch/69. Sqrt(x)/code.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int start = 0;
+        int end = x;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if((long) mid * mid < (long) x){
+                start = mid + 1;
+            }
+            else if((long) mid * mid > (long) x){
+                end = mid - 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return end;
+    }
+};
