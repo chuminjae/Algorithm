1173d595000e69938c158a1a314dfe9091a99d34
c_vgvrthrg <choochootrain1130@gmail.com>
2025-08-28 11:55:00 +0900

    Create code.cpp

---
+++ Binarysearch/74.Search a 2D matrix/code.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int start = 0;
+        int end = matrix.size() * matrix[0].size() - 1;
+        int mid = -1;
+        while(start <= end){
+            mid = start + (end - start) / 2;
+            pair<int, int> position = convert(matrix[0].size(), mid);
+            if(matrix[position.first][position.second] > target){
+                end = mid - 1;
+            }
+            else if(matrix[position.first][position.second] < target){
+                start = mid + 1;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+    pair<int, int> convert(int n, int a){
+        pair<int, int> position;
+        position.first = a / n;
+        position.second = a % n;
+        return position;
+    }
+};
